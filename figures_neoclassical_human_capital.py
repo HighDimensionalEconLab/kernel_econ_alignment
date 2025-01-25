550c95bfc4ea0ae846c245dcdaadeb3569a36bac
Mahdi E Kahou (Mahdi Ebrahimi Kahou) <43940086+Mekahou@users.noreply.github.com>
2025-01-25 18:45:46 -0500

    Human capital done

--- figures_neoclassical_human_capital.py
+++ figures_neoclassical_human_capital.py
@@ -9,9 +9,9 @@ from mpl_toolkits.axes_grid1.inset_locator import (
     inset_axes,
 )
 
-fontsize = 14
-ticksize = 14
-figsize = (15, 15)
+fontsize = 17
+ticksize = 16
+figsize = (15, 18)
 params = {
     "font.family": "serif",
     "figure.figsize": figsize,
@@ -37,60 +37,71 @@ k_hat = sol["k_test"]
 h_hat = sol["h_test"]
 i_k_hat = sol["i_k_test"]
 i_h_hat = sol["i_h_test"]
-mu_hat = sol["mu_test"]
+mu_k_hat = sol["mu_k_test"]
+mu_h_hat = sol["mu_h_test"]
+
 # Plotting
 
-ax_physical_capital = plt.subplot(3, 2, 1)
+ax_physical_capital = plt.subplot(4, 2, 1)
 
-plt.plot(t, k_hat, color="k", label=r"$\hat{x}_k(t)$")
+plt.plot(t, k_hat, color="k", label=r"$\hat{x}_k(t)$: Kernel Approximation")
 #plt.axhline(y=sol["k_ss"], linestyle="-.", color="k", label=r"$k^*$:Steady-State")
 plt.axvline(x=T, color="k", linestyle=":", label="Extrapolation/Interpolation")
 plt.ylabel("Physical Capital: $x_k(t)$")
 plt.xlabel("Time")
 plt.legend()  # Show legend with labels
 
-ax_human_capital = plt.subplot(3, 2, 2)
+ax_human_capital = plt.subplot(4, 2, 2)
 
-plt.plot(t, h_hat, color="k", label=r"$\hat{x}_h(t)$")
+plt.plot(t, h_hat, color="k", label=r"$\hat{x}_h(t)$: Kernel Approximation")
 #plt.axhline(y=sol["h_ss"], linestyle="-.", color="grey", label=r"$h^*$: Steady-State")
 plt.axvline(x=T, color="k", linestyle=":", label="Extrapolation/Interpolation")
 plt.ylabel("Human Capital: $x_h(t)$")
 plt.xlabel("Time")
 plt.legend()  # Show legend with labels
 
-ax_consumption = plt.subplot(3, 2, 3)
+ax_consumption = plt.subplot(4, 2, 3)
 
-plt.plot(t, c_hat, color="b", label=r"$\hat{y}_c(t)$")
+plt.plot(t, c_hat, color="b", label=r"$\hat{y}_c(t)$: Kernel Approximation")
 #plt.axhline(y=sol["c_ss"], linestyle="-.", color="b", label=r"$c^*$: Steady-State")
 plt.axvline(x=T, color="b", linestyle=":", label="Extrapolation/Interpolation")
 plt.ylabel("Consumption: $y_c(t)$")
 plt.xlabel("Time")
 plt.legend()  # Show legend with labels
 
-ax_investment_k = plt.subplot(3, 2, 4)
+ax_investment_k = plt.subplot(4, 2, 4)
 
-plt.plot(t, i_k_hat, color="b", label=r"$\hat{y}_k(t)$")
+plt.plot(t, i_k_hat, color="b", label=r"$\hat{y}_k(t)$: Kernel Approximation")
 #plt.axhline(y=sol["i_k_ss"], linestyle="-.", color="k", label=r"$i_k^*$: Steady-State")
 plt.axvline(x=T, color="b", linestyle=":", label="Extrapolation/Interpolation")
 plt.ylabel("Physical Capital Investment: $y_k(t)$")
 plt.xlabel("Time")
 plt.legend()  # Show legend with labels
 
-ax_investment_h = plt.subplot(3, 2, 5)
+ax_investment_h = plt.subplot(4, 2, 5)
 
-plt.plot(t, i_h_hat, color="b", label=r"$\hat{y}_h(t)$")
+plt.plot(t, i_h_hat, color="b", label=r"$\hat{y}_h(t)$: Kernel Approximation")
 #plt.axhline(y=sol["i_h_ss"], linestyle="-.", color="grey", label=r"$i_h^*$: Steady-State")
 plt.axvline(x=T, color="b", linestyle=":", label="Extrapolation/Interpolation")
 plt.ylabel("Human Capital Investment: $y_h(t)$")
 plt.xlabel("Time")
 plt.legend()  # Show legend with labels
 
-ax_investment_h = plt.subplot(3, 2, 6)
+ax_investment_mu_k = plt.subplot(4, 2, 6)
 
-plt.plot(t, mu_hat, color="grey", label=r"$\hat{\mu}(t)$")
+plt.plot(t, mu_k_hat, color="grey", label=r"$\hat{\mu}_k(t)$: Kernel Approximation")
 #plt.axhline(y=sol["i_h_ss"], linestyle="-.", color="grey", label=r"$i_h^*$: Steady-State")
 plt.axvline(x=T, color="grey", linestyle=":", label="Extrapolation/Interpolation")
-plt.ylabel("Co-state Variable: $\mu(t)$")
+plt.ylabel("Co-state Variable: $\mu_k(t)$")
+plt.xlabel("Time")
+plt.legend()  # Show legend with labels
+
+ax_investment_mu_h = plt.subplot(4, 2, 7)
+
+plt.plot(t, mu_h_hat, color="grey", label=r"$\hat{\mu}_h(t)$: Kernel Approximation")
+#plt.axhline(y=sol["i_h_ss"], linestyle="-.", color="grey", label=r"$i_h^*$: Steady-State")
+plt.axvline(x=T, color="grey", linestyle=":", label="Extrapolation/Interpolation")
+plt.ylabel("Co-state Variable: $\mu_h(t)$")
 plt.xlabel("Time")
 plt.legend()  # Show legend with labels
 
--- old code/b_neoclassical_human_capital_matern.py
+++ old code/b_neoclassical_human_capital_matern.py
@@ -21,10 +21,12 @@ def human_capital_matern(
     k_0: float = 1.5, #3.0024724187979452, #1.5,
     nu: float = 0.5,
     sigma: float = 1.0,
-    rho: float = 10,
+    rho: float = 20,
+    lambda_b_k: float = 1.0,
+    lambda_b_h: float = 1.0,
     solver_type: str = "ipopt",
     train_T: float = 80.0,
-    train_points: int = 81,
+    train_points: int = 60,
     test_T: float = 100.0,
     test_points: int = 100,
     benchmark_T: float = 60.0,
@@ -71,18 +73,20 @@ def human_capital_matern(
     # 8 variables
     m.alpha_k = pyo.Var(m.I, within=pyo.Reals, initialize = 0.0) # coeffs for x_k(t): we call it in the code k(t)
     m.alpha_h = pyo.Var(m.I, within=pyo.Reals, initialize = 0.0) # coeffs for x_h(t): we call it in the code h(t)
-    m.alpha_mu_k = pyo.Var(m.I, within=pyo.Reals, initialize = 0.0)  # coeffs for \mu_k(t)
-    m.alpha_mu_h = pyo.Var(m.I, within=pyo.Reals, initialize = 0.0)  # coeffs for \mu_h(t)
+    m.alpha_mu = pyo.Var(m.I, within=pyo.Reals, initialize = 0.0)  # coeffs for \mu(t)
     m.alpha_i_k = pyo.Var(m.I, within=pyo.Reals, initialize = 0.0) # coeff for y_k(t): we call it i_k(t) in the code
     m.alpha_i_h = pyo.Var(m.I, within=pyo.Reals, initialize = 0.0) # coeff for y_h(t): we call it i_h(t) in the code
     m.alpha_c = pyo.Var(m.I, within=pyo.Reals, initialize = 0.0) # coeff for for y_c(t):  we call it c(t) in the code
-    
+    m.alpha_b_k = pyo.Var(m.I, within=pyo.Reals, initialize=0.0) # coeff for b_k(t) = \mu(t)x_k(t)
+    m.alpha_b_h = pyo.Var(m.I, within=pyo.Reals, initialize=0.0) # coeff for b_k(t) = \mu(t)x_h(t)
+
     #initializations of variables at 0 for 6 variables: h_0 and k_0 are given to us from the economic setup
     m.i_k_0 = pyo.Var(within=pyo.NonNegativeReals, initialize=delta_k * k_0) #y_k(0)
     m.i_h_0 = pyo.Var(within=pyo.NonNegativeReals, initialize=delta_h * h_0)#y_h(0)
     m.c_0 = pyo.Var(within=pyo.NonNegativeReals, initialize = f(k_0, h_0) - delta_h * h_0 - delta_k * k_0) #y_c(0)
-    m.mu_k_0 = pyo.Var(within=pyo.NonNegativeReals, initialize = 1/(f(k_0, h_0) - delta_h * h_0 - delta_k * k_0)) #\mu(0) = 1/c(0) 
-    m.mu_h_0 = pyo.Var(within=pyo.NonNegativeReals, initialize = 1/(f(k_0, h_0) - delta_h * h_0 - delta_k * k_0)) #\mu(0) = 1/c(0) 
+    m.mu_0 = pyo.Var(within=pyo.NonNegativeReals, initialize = 1/(f(k_0, h_0) - delta_h * h_0 - delta_k * k_0)) #\mu(0) = 1/c(0) 
+    m.b_k_0 = pyo.Var(within=pyo.NonNegativeReals, initialize=k_0/(f(k_0, h_0) - delta_h * h_0 - delta_k * k_0)) # b_k_0 = m(0)k(0)
+    m.b_h_0 = pyo.Var(within=pyo.NonNegativeReals, initialize=h_0/(f(k_0, h_0) - delta_h * h_0 - delta_k * k_0)) #b_h_0 = m(0)k(0)
 
 
     # defining the integrated kernel functional approximation
@@ -101,13 +105,14 @@ def human_capital_matern(
     def c(m, i): #y_c(t): consumption, c(t)
         return m.c_0 + sum(K_tilde[i, j] * m.alpha_c[j] for j in m.I)
     
-    def mu_k(m, i): #\mu(t): co-state variable
-        return m.mu_k_0 + sum(K_tilde[i, j] * m.alpha_mu_k[j] for j in m.I)
+    def mu(m, i): #\mu(t): co-state variable
+        return m.mu_0 + sum(K_tilde[i, j] * m.alpha_mu[j] for j in m.I)
 
-    def mu_h(m, i): #the transversality variable for physical capital
-        return m.mu_h_0 + sum(K_tilde[i, j] * m.alpha_mu_h[j] for j in m.I)
+    def b_k(m, i): #the transversality variable for physical capital
+        return m.b_k_0 + sum(K_tilde[i, j] * m.alpha_b_k[j] for j in m.I)
     
- 
+    def b_h(m, i): #the transversality variable for human capital
+        return m.b_h_0 + sum(K_tilde[i, j] * m.alpha_b_h[j] for j in m.I)
 
     # defining the derivatives of the variables
     def dk_dt(m, i):  # \dot{x}_k(t) : derivative of the physical capital
@@ -116,11 +121,9 @@ def human_capital_matern(
     def dh_dt(m, i): # \dot{x}_h(t) : derivative of the human capital
         return sum(K[i, j] * m.alpha_h[j] for j in m.I)
     
-    def dmu_k_dt(m, i):
-        return sum(K[i, j] * m.alpha_mu_k[j] for j in m.I)
-    
-    def dmu_h_dt(m, i):
-        return sum(K[i, j] * m.alpha_mu_h[j] for j in m.I)
+    def dmu_dt(m, i):
+        return sum(K[i, j] * m.alpha_mu[j] for j in m.I)
+
     
 
     # defining  constraints and objective for model and solve
@@ -133,13 +136,12 @@ def human_capital_matern(
         return dh_dt(m, i) == i_h(m, i) - delta_h * h(m, i)
 
     @m.Constraint(m.I)  # for each index in m.I
-    def dmu_k_dt_constraint(m, i):
-        return dmu_k_dt(m, i) == -mu_k(m, i) * (f_k(k(m, i), h(m, i)) - delta_k - rho_hat)
+    def dmu_dt_constraint(m, i):
+        return dmu_dt(m, i) == -mu(m, i) * (f_k(k(m, i), h(m, i)) - delta_k - rho_hat)
     
     @m.Constraint(m.I)  # for each index in m.I
-    def dmu_h_dt_constraint(m, i):
-        return dmu_h_dt(m, i) == -mu_h(m, i) * (f_h(k(m, i), h(m, i)) - delta_h - rho_hat)
-    
+    def no_arbitrage(m, i):
+        return 0.0 == f_h(k(m, i), h(m, i)) - f_k(k(m, i), h(m, i)) - delta_h + delta_k
     
     @m.Constraint(m.I)  # for each index in m.I
     def feasibility(m, i):
@@ -147,16 +149,19 @@ def human_capital_matern(
     
     @m.Constraint(m.I)  # for each index in m.I
     def shadow_price(m, i):
-        return mu_k(m, i) * c(m, i) -1.0 == 0.0
+        return mu(m, i) * c(m, i) -1.0 == 0.0
     
+    @m.Constraint(m.I)  # for each index in m.I
+    def b_k_constraint(m, i):
+        return mu(m, i) * k(m, i) == b_k(m, i)
     
     @m.Constraint(m.I)  # for each index in m.I
     def b_h_constraint(m, i):
-        return mu_k(m, i) - mu_h(m, i) == 0.0
+        return mu(m, i) * h(m, i) == b_h(m, i)
 
     @m.Objective(sense=pyo.minimize)
     def min_norm(m):  # alpha @ K @ alpha not supported by pyomo
-        return sum(K[i, j] * m.alpha_mu_k[i] * m.alpha_mu_k[j] for i in m.I for j in m.I) + sum(K[i, j] * m.alpha_k[i] * m.alpha_k[j] for i in m.I for j in m.I)+ sum(K[i, j] * m.alpha_mu_h[i] * m.alpha_mu_h[j] for i in m.I for j in m.I)+sum(K[i, j] * m.alpha_h[i] * m.alpha_h[j] for i in m.I for j in m.I)
+        return lambda_b_k * sum(K[i, j] * m.alpha_b_k[i] * m.alpha_b_k[j] for i in m.I for j in m.I) + lambda_b_h * sum(K[i, j] * m.alpha_b_h[i] * m.alpha_b_h[j] for i in m.I for j in m.I)
 
     solver = pyo.SolverFactory(solver_type)
     options = (
@@ -173,16 +178,12 @@ def human_capital_matern(
     alpha_h = jnp.array([pyo.value(m.alpha_h[i]) for i in m.I])
     alpha_i_k = jnp.array([pyo.value(m.alpha_i_k[i]) for i in m.I])
     alpha_i_h = jnp.array([pyo.value(m.alpha_i_h[i]) for i in m.I])
-    alpha_mu_k = jnp.array([pyo.value(m.alpha_mu_k[i]) for i in m.I])
-    alpha_mu_h = jnp.array([pyo.value(m.alpha_mu_h[i]) for i in m.I])
-
+    alpha_mu = jnp.array([pyo.value(m.alpha_mu[i]) for i in m.I])
 
     c_0 = pyo.value(m.c_0)
     i_k_0 = pyo.value(m.i_k_0)
     i_h_0 = pyo.value(m.i_h_0)
-    mu_k_0 = pyo.value(m.mu_k_0)
-    mu_h_0 = pyo.value(m.mu_h_0)
-
+    mu_0 = pyo.value(m.mu_0)
     # Interpolator using training data
     #@jax.jit
     def kernel_solution(test_data):
@@ -195,14 +196,12 @@ def human_capital_matern(
         h_test = h_0 + K_tilde_test @ alpha_h
         i_k_test = i_k_0 + K_tilde_test @ alpha_i_k
         i_h_test = i_h_0 + K_tilde_test @ alpha_i_h
-        mu_k_test = mu_k_0 + K_tilde_test @ alpha_mu_k
-        mu_h_test = mu_h_0 + K_tilde_test @ alpha_mu_h
-
+        mu_test = mu_0 + K_tilde_test @ alpha_mu
         feasibility_test = c_test + i_h_test + i_k_test - f(k_test, h_test)
-        return k_test, h_test, c_test, i_k_test, i_h_test, mu_k_test, mu_h_test, feasibility_test
+        return k_test, h_test, c_test, i_k_test, i_h_test, mu_test, feasibility_test
 
     # Generate test_data
-    k_test, h_test, c_test, i_k_test, i_h_test, mu_k_test, mu_h_test, feasibility_test = kernel_solution(test_data)
+    k_test, h_test, c_test, i_k_test, i_h_test, mu_test, feasibility_test = kernel_solution(test_data)
 
     print(
         f"solve_time(s) = {results.solver.Time}"
@@ -215,8 +214,7 @@ def human_capital_matern(
         "c_test": c_test,
         "i_k_test": i_k_test,
         "i_h_test": i_h_test,
-        "mu_k_test": mu_k_test,
-        "mu_h_test": mu_h_test,
+        "mu_test": mu_test,
         "feasibility_test": feasibility_test,
         "alpha_c": alpha_c,
         "alpha_k": alpha_k,
