13cee533c7fcce7f7f101076f3d8a7602e27516b
Wuyang Ren <waynrn@outlook.com>
2025-10-20 16:00:10 -0700

    julia port first step

--- src/KernelEconExamples.jl
+++ src/KernelEconExamples.jl
@@ -1,6 +1,13 @@
 module KernelEconExamples
-using Distributions
+using Distributions, QuadGK
 
 include("kernels.jl")
-export matern_kernel_0p5
+export matern_kernel_0p5, 
+    matern_kernel_1p5,
+    matern_kernel_2p5,
+    matern_kernel_inf,
+    integrated_matern_kernel_0p5,
+    integrated_matern_kernel_1p5,
+    integrated_matern_kernel_2p5,
+    integrated_matern_kernel_inf
 end
