6b8bd902eb43c2c33ed97c8f5ef550ac14323870
Wuyang Ren <waynrn@outlook.com>
2025-10-20 13:42:26 -0700

    revert commits; accidentally pushed to main branch
    This reverts commit 9914d77da69d20000297cd94fa5edd4d9e2bf59d.

--- src/KernelEconExamples.jl
+++ src/KernelEconExamples.jl
@@ -1,13 +1,6 @@
 module KernelEconExamples
-using Distributions, QuadGK
+using Distributions
 
 include("kernels.jl")
-export matern_kernel_0p5, 
-    matern_kernel_1p5,
-    matern_kernel_2p5,
-    matern_kernel_inf,
-    integrated_matern_kernel_0p5,
-    integrated_matern_kernel_1p5,
-    integrated_matern_kernel_2p5,
-    integrated_matern_kernel_inf
+export matern_kernel_0p5
 end
