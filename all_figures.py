f37010ec625f8f6914afd85e791f554c2a09293b
Jesse Perla <jesseperla@gmail.com>
2025-10-23 09:34:46 -0700

    Display of figures and added something to gitignore on output.

--- all_figures.py
+++ all_figures.py
@@ -6,10 +6,23 @@ output_folder = os.path.abspath(
 os.makedirs(output_folder, exist_ok=True)
 
 # run all the figures scripts
+print("Executing figures_asset_pricing.py")
 os.system("python figures_asset_pricing.py")
+
+print("Executing figures_neoclassical_growth_concave_convex.py")
 os.system("python figures_neoclassical_growth_concave_convex.py")
-os.system("python figures_neoclassical_human_capital.py")
+
+print("Executing figures_neoclassical_growth_baseline.py")
 os.system("python figures_neoclassical_growth_baseline.py")
+
+print("Executing figures_neoclassical_growth_robustness.py")
 os.system("python figures_neoclassical_growth_robustness.py")
+
+print("Executing figures_optimal_advertising.py")
 os.system("python figures_optimal_advertising.py")
+
+print("Executing tables_neoclassical_growth.py")
 os.system("python tables_neoclassical_growth.py")
+
+print("Executing figures_neoclassical_human_capital.py")
+os.system("python figures_neoclassical_human_capital.py")
